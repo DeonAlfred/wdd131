6ecfd81b00defa41da1557b8bce5d98b3cc3d93d
DeonAlfred <deonjohannes1984@gmail.com>
2024-10-17 23:30:08 +0200

    commit

---
+++ scripts/getdates.js
@@ -0,0 +1,8 @@
+document.getElementById('d').innerHTML = Date();
+var a = 2 * 2;
+let ds = 'Daniell Johannes'
+z = a + ds;
+document.getElementById('s').innerHTML = z;
+document.querySelector('.pics').addEventListener('click', function () {
+    this.classList.toggle('spin');
+});
